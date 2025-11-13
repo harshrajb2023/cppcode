d524131a7fd4cf1a4ae119a279619131d5a90f0d
Harsh Raj Bind <121737517+harshrajb2023@users.noreply.github.com>
2025-11-13 16:39:16 +0530

    Update a.cpp

--- STL/a.cpp
+++ STL/a.cpp
@@ -1,30 +1,63 @@
+//define and output
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     
-    map<pair<string,string>,vector<int>> mpv;
-    set<pair<int,int>> sp;
+    map<pair<string,string>,vector<int>> mpv;  // key = (first name, last name), value = list of ints
+    set<pair<int,int>> sp;                     // (unused here)
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        string fn,ln;
+    cin >> n;  // number of persons
+    
+    for(int i = 0; i < n; i++){
+        string fn, ln;
         int ct;
-        cin>>fn>>ln>>ct;
-        for(int j=0;j<ct;j++){
+        cin >> fn >> ln >> ct;  // read first name, last name, and count of numbers
+        
+        for(int j = 0; j < ct; j++){
             int x;
-            cin>>x;
-            mpv[{fn,ln}].push_back(x);
+            cin >> x;
+            mpv[{fn, ln}].push_back(x);  // store numbers in vector for that (fn, ln)
         }
     }
 
-    for(auto value:mpv){
-        auto &full_name=value.first;
-        auto &list=value.second;
-        for(auto element:list){
-            cout<<element<<" ";
+    // Print stored data
+    for(auto value : mpv){
+        auto &full_name = value.first;
+        auto &list = value.second;
+        for(auto element : list){
+            cout << element << " ";
         }
-        cout<<endl;
-        
+        cout << endl;
     }
 }
+
+INPUT
+2
+Harsh Raj 3 10 20 30
+Amit Singh 2 5 15
+
+
+
+OUTPUT
+5 15 
+10 20 30 
+
+
+#2
+     //change  Line number 26 to 34
+    // Print stored data
+    for(auto value : mpv){
+        auto &full_name = value.first;
+        auto &list = value.second;
+        cout << full_name.first << " " << full_name.second << ": ";
+        for(auto element : list){
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+
+OUTPUT
+Amit Singh: 5 15 
+Harsh Raj: 10 20 30 
